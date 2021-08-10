5f2b3836cfb851a7cbf038947192db3898368003
wenqianKK <18041151259@163.com>
2021-08-11 01:01:42 +0800

    响应式

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React, { useState } from 'react';
 import { Link } from 'gatsby'
 import {
   logo,
@@ -6,13 +6,16 @@ import {
   header,
   nav,
   navActive,
-  imgLogo
-
+  imgLogo,
+  navbarToggle,
+  srOnly,
+  iconBar
 } from './index.module.css'
 // import { StaticImage } from 'gatsby-plugin-image'
 import logoImg from "../../images/logo.png"
 
 const Header = ({ pageTitle, children }) => {
+  const [navVisible, setNavVisible] = useState(false);
   return (
     <header className={header}>
       <div className={logo}>
@@ -26,7 +29,17 @@ const Header = ({ pageTitle, children }) => {
           src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
         /> */}
       </div>
-      <nav className={nav}>
+      <button 
+        type="button" 
+        data-toggle="collapse"  
+        className={navbarToggle}
+        onClick={()=>{setNavVisible(!navVisible)}}
+      >
+        <span className={iconBar}></span>
+        <span className={iconBar}></span>
+        <span className={iconBar}></span>
+      </button>
+      <nav className={nav} style={navVisible ? {display:"flex"}:{}}>
         <ul className={navLinks}>
           <li><Link to="/" activeClassName={navActive}>首页</Link></li>
           <li><Link to="/solution" activeClassName={navActive}>解决方案</Link></li>
