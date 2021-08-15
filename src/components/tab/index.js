fba084da68fbb7cf3cb6955a77afe5e10a9b1c6b
wenqianKK <18041151259@163.com>
2021-08-15 10:25:36 +0800

    modify product details active tab

--- src/components/tab/index.js
+++ src/components/tab/index.js
@@ -1,4 +1,5 @@
-import * as React from 'react'
+import React, { useState ,useEffect} from 'react';
+
 import {
   tabWrapper,
   tabContainer,
@@ -9,19 +10,23 @@ import {
 } from './index.module.css'
 
 const Tab = ({ dataSource,active ,onChange,style={}}) => {
+  const [activeTab, setActive] = useState(active);
 
   const activeTabStyle = {
     borderBottom:"3px solid #00B996",
     color:"#00B996"
   }
+  useEffect(() => {
+    setActive(active)
+  }, [active])
 
   return (
     <div className={tabWrapper} style={style}>
       <div className={tabContainer}>
         {
           dataSource && dataSource.length>0 && dataSource.map (item => 
-          <div className={tabItem} style={active ===  item.value ? activeTabStyle:{}} onClick={()=>{onChange(item.value)}}>
-            {item.icon && <img src={active === item.value ? item.activeIcon: item.icon} alt="icon" className={tabIcon}/>}
+          <div className={tabItem} style={activeTab ===  item.value ? activeTabStyle:{}} onClick={()=>{onChange(item.value)}}>
+            {item.icon && <img src={activeTab === item.value ? item.activeIcon: item.icon} alt="icon" className={tabIcon}/>}
             <span className={tabContent}>{item.label}</span>
           </div>)
         }
